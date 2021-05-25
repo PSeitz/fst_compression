ef7302f8b56fc34ced13f578587e1c412eb71dad
Pascal Seitz <pascal.seitz@gmail.com>
2021-05-25 09:20:26 +0200

    add dickens test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
+use std::fs;
+
 use fst::Result;
 use fst::{MapBuilder, SetBuilder};
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 use uuid::Uuid;
-
 fn main() -> Result<()> {
-    let my_uuid = Uuid::new_v4();
-    println!("{}", my_uuid.to_simple());
+    // dickens
+    let mut terms = vec![];
+    let file = File::open("dickens.txt")?;
+    let reader = io::BufReader::new(file);
 
-    let mut uuids = vec![];
-
-    for _ in 0..256_000 {
-        uuids.push(Uuid::new_v4().to_simple().to_string());
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            terms.push(line.to_string());
+        }
     }
 
-    let total_size = uuids.iter().map(|uuid| uuid.len()).sum::<usize>();
-    dbg!(total_size);
-    uuids.sort();
+    // uuid
+    //let my_uuid = Uuid::new_v4();
+    //println!("{}", my_uuid.to_simple());
+    //let mut terms = vec![];
+    //for _ in 0..256_000 {
+    //terms.push(Uuid::new_v4().to_simple().to_string());
+    //}
 
-    use std::fs::File;
-    use std::io;
+    terms.sort();
+    terms.dedup();
+    let total_size = terms.iter().map(|uuid| uuid.len()).sum::<usize>();
+    dbg!(total_size);
+    dbg!(terms.len());
 
     // This is where we'll write our map to.
     let map_wtr = io::BufWriter::new(File::create("map.fst")?);
@@ -26,7 +38,7 @@ fn main() -> Result<()> {
     // Create a builder that can be used to insert new key-value pairs.
     let mut mapbuild = MapBuilder::new(map_wtr)?;
     let mut setbuild = SetBuilder::new(set_wtr)?;
-    for (id, term) in uuids.iter().enumerate() {
+    for (id, term) in terms.iter().enumerate() {
         mapbuild.insert(term, id as u64).unwrap();
         setbuild.insert(term).unwrap();
     }
